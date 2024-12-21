f687a604aa3a3b0c203557e506d216f05b08b9f2
alexszrosa <aleawsai@gmail.com>
2024-12-21 20:06:58 -0300

    Alteração no calculo do juros

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.title("Cálculo do Valor do tratamento")
+
+st.divider()
+
+Vlr_Servico = st.number_input("Digite o valor do serviço: ")
+
+st.divider()
+
+Taxa = st.number_input("Digite a Taxa: ")
+
+st.divider()
+
+Prazo = st.number_input("Digite o prazo: ")
+
+st.divider()
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Juros Simples")
+    if Prazo > 1:
+        Vlr_parcela = (Vlr_Servico+(Vlr_Servico*Taxa/100))/Prazo
+        Vlrs = st.write(f"O valor de cada parcela R${Vlr_parcela:.2f} ")
+        st.balloons()
+    else:
+        "Digite o prazo maior que zero"
+
+with col2:
+    st.header("Juros Compostos")
+    if Prazo > 1:
+        Taxac = (1+Taxa/100) ** Prazo
+        Vlr_parcelac = (Vlr_Servico*Taxac)/(int(Taxa)/100+Prazo)
+        Vlrc = st.write(f"O valor de cada parcela R${Vlr_parcelac:.2f} ")
+        st.balloons()
+    else:
+        "Digite o prazo maior que zero"     
+
+st.divider()
